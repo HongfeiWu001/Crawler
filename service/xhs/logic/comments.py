0fda1b0d834cedeffe0ee51db7f7d13d39a28b9f
hongfeiwu <hongfeiwu@hkaift.com>
2024-12-31 12:07:53 +0800

    init

--- service/xhs/logic/comments.py
+++ service/xhs/logic/comments.py
@@ -1,10 +1,13 @@
 from .common import common_request
 
-async def request_comments(id: str, cookie: str, offset: int, limit: int) -> tuple[dict, bool]:
+
+async def request_comments(
+    id: str, cookie: str, offset: int, limit: int
+) -> tuple[dict, bool]:
     """
     请求小红书获取评论信息
     """
-    cursor = ''
+    cursor = ""
     headers = {"cookie": cookie}
     end_length = offset + limit
     comments = []
@@ -13,15 +16,17 @@ async def request_comments(id: str, cookie: str, offset: int, limit: int) -> tup
         data = {
             "note_id": id,
             "cursor": cursor,
-            "top_comment_id": '',
-            "image_formats": ["jpg", "webp", "avif"]
+            "top_comment_id": "",
+            "image_formats": ["jpg", "webp", "avif"],
         }
-        resp, succ = await common_request('/api/sns/web/v2/comment/page', data, headers, True, False)
+        resp, succ = await common_request(
+            "/api/sns/web/v2/comment/page", data, headers, True, False
+        )
         if not succ:
             return {}, succ
-        has_more = resp.get('data', {}).get('has_more', False)
-        comments.extend(resp.get('data', {}).get('comments', []))
-        cursor = resp.get('data', {}).get('cursor', '')
+        has_more = resp.get("data", {}).get("has_more", False)
+        comments.extend(resp.get("data", {}).get("comments", []))
+        cursor = resp.get("data", {}).get("cursor", "")
 
     ret = {"comments": comments[offset:end_length]}
     return ret, succ
--- service/xhs/views/detail.py
+++ service/xhs/views/detail.py
@@ -1,26 +1,31 @@
-from utils.error_code import ErrorCode
-from utils.reply import reply
-from ..models import accounts
-from lib.logger import logger
-from ..logic import request_detail
 import random
 
+from lib.logger import logger
+from utils.error_code import ErrorCode
+from utils.reply import reply
+
+from ..logic import request_detail
+from ..models import accounts
+
+
 # route
-async def detail(id: str):
+async def detail(id: str, xsec_token: str):
     """
     获取笔记信息
     """
     _accounts = await accounts.load()
     random.shuffle(_accounts)
     for account in _accounts:
-        if account.get('expired', 0) == 1:
+        if account.get("expired", 0) == 1:
             continue
-        account_id = account.get('id', '')
-        res, succ = await request_detail(id, account.get('cookie', ''))
+        account_id = account.get("id", "")
+        res, succ = await request_detail(id, xsec_token, account.get("cookie", ""))
         if res == {} or not succ:
-            logger.error(f'get note detail failed, account: {account_id}, id: {id}')
+            logger.error(f"get note detail failed, account: {account_id}, id: {id}")
             continue
-        logger.info(f'get note detail success, account: {account_id}, id: {id}, res: {res}')
-        return reply(ErrorCode.OK, '成功' , res)
-    logger.warning(f'get note detail failed. id: {id}')
-    return reply(ErrorCode.NO_ACCOUNT, '请先添加账号')
+        logger.info(
+            f"get note detail success, account: {account_id}, id: {id}, res: {res}"
+        )
+        return reply(ErrorCode.OK, "成功", res)
+    logger.warning(f"get note detail failed. id: {id}")
+    return reply(ErrorCode.NO_ACCOUNT, "请先添加账号")
